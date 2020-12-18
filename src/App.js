5fd43b9e55b6021745e5ca7f105a28c5d0c9ae76
Suresh Kumar <suresh.palani@abjayon.com>
2020-12-18 13:14:05 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Amplify from "aws-amplify";
+//import Auth from '@aws-amplify/auth'; this not working
+import "@aws-amplify/ui/dist/style.css";
+import { withAuthenticator } from "aws-amplify-react"; // or 'aws-amplify-react-native';
+import aws_exports from "./aws-exports";
+
+//import { API } from "aws-amplify";
+
+// in this way you are only importing Auth and configuring it.
+Amplify.configure(aws_exports);
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello, World!</p>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App, true);
